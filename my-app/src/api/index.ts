adf080358e69e463b27de435d107876fc090ac70
Mickey <mickey@Mickeys-MacBook-Air-2.local>
2025-04-20 18:03:49 +0700

    fix: add api

--- my-app/src/api/index.ts
+++ my-app/src/api/index.ts
@@ -1,6 +1,10 @@
+import axios from "axios";
+
 export const apiGetPrice = async () => {
-  return {
-    price: 10,
-    lastUpdate: 11,
-  };
+  try {
+    const response = await axios.get("http://localhost:3000/api/bnb-price");
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed to fetch BNB price");
+  }
 };
